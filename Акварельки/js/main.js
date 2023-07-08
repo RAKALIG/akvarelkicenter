c523ba38f37eab8a2059ef569a8450965216e373
RAKALIG <122606350+RAKALIG@users.noreply.github.com>
2023-07-08 18:58:23 +0300

    akvarelki

---
+++ Акварельки/js/main.js
@@ -0,0 +1,9 @@
+$('.open-popup').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg').fadeIn(600);
+    $('html').addClass('no-scroll');
+})
+$('.close-popup').click(function() {
+    $('.popup-bg').fadeOut(600);
+    $('html').removeClass('no-scroll');
+})
